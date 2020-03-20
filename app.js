952b44122eb15479c71faeb9401ffe26b8f33c91
MurtazaKhosa <murtazakhoso0316@gmail.com>
2020-03-20 05:42:03 -0400

    js assigment

---
+++ app.js
@@ -0,0 +1,159 @@
+// // alert("hello i am alert box")
+
+// // alert("hello MR,bean.\n how are you")
+
+// var hello = ('hello world')
+
+// var braek = ('A smarter way to learn')
+// console.log('hello,braek')
+
+// var message = ('hello world')
+// alert('message')
+
+// var book = ('A smarter way to learn')
+// document.write(book)
+
+// var age = ('i am 19 years old')
+// alert(age)
+
+// var birthyear = ('i was born in 11/0ct/2001')
+// document.write(birthyear)
+
+// var a
+// alert(a)
+// a = 10
+// alert(a)
+// a++
+// alert
+// (a)
+// a+=7
+// alert(a)
+// a--
+// alert(a)
+// b = 3
+// a/b
+// alert(b)
+// var c = 2, d = 1 ;
+// --c;
+// --c - --d;
+// --c - --d + ++d;
+// --c - --d + ++d + d--;
+// alert(c)
+
+
+// function calculateTemp() {
+//     function celToFah(celsius) {
+//        var fahrenheit = Math.round((celsius * (9/5)) + 32);
+//        return fahrenheit;
+//     }
+ 
+//     function fahToCel(fahrenheit) {
+//        var celsius = Math.round((fahrenheit - 32) * (5/9));
+//        return celsius;
+//     }
+ 
+//     // Checks if Calsius or Fahrenheit is selected
+//     var tempSelected = document.getElementById("temp_diff");
+//     var valueTemp = tempSelected.options[tempSelected.selectedIndex].value;
+ 
+//     // Checks the value inside the Temperature input
+//     var numberTemp = document.getElementById("temp").value;
+ 
+//     var result;
+    
+//     if (valueTemp == 1) {
+//        result = celToFah(numberTemp);
+//        document.getElementById("resultContainer").innerHTML = "= " + result + "°Fahrenheit";
+//     } else {
+//        result = fahToCel(numberTemp);
+//        document.getElementById("resultContainer").innerHTML = "= " + result + "°Celsius";
+//     }
+    
+//  }
+// var  murtaza = prompt('Enter your name');
+// alert(murtaza);
+
+// var gender = prompt('Gender');
+// if (gender==='male')
+// {
+//     alert('Good morning sir')
+// }
+// else if (gender==='female')
+// {
+//     alert('Good morning ma,am')
+// }
+// else
+// {
+//     alert('hy buddy')
+// }
+
+// var color = prompt('signal color')
+// if(color === 'red'){
+//     alert('Must stop')
+// }
+// else if(color === 'yellow'){
+//     alert('Ready to move')
+// }
+// else if (color === 'green'){
+//     alert('move Now')
+// }
+// else{
+//     alert('color not found')
+// }
+
+// var vowel = prompt('Enter any vowel');
+// if (vowel === 'a')
+// {
+//     alert('True')
+// }
+// else if(vowel === 'e'){
+//     alert('True')
+// }
+// else if(vowel === 'i'){
+//     alert('True')
+// }
+// else if(vowel === 'o'){
+//     alert('True')
+// }
+// else if(vowel === 'u'){
+//     alert('True')
+// }
+// else{
+//     alert('flase')
+// }
+
+// var index = prompt("start from");
+// var endpoin = prompt("end point");
+// var table = prompt("which table");
+// for (var index = 1 ; index <=endpoin; index++){
+
+//     document.write(table+" x "+index+" ="+index*table+"<br>");
+// }
+// var counting = prompt("counting")
+// for(var index = 0; index <=10; index++){
+//     document.write(index +"<br>")
+// }
+
+// var Rcounting = prompt("reverse counting")
+// for (var index = 15; index >=0; index--){
+//     document.write(index+"<br>")
+// }
+
+// var Even = prompt('even number');
+// for(var index = 0; index <=20; index+=2){
+//     document.write(index+"</br>")
+// }
+
+// var odd = prompt("odd")
+// for(index = 1; index <=19; index+=2){
+//     document.write(index + "<br>")
+// }
+
+
+
+
+
+
+
+
+
